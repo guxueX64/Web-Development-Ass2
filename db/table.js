79444bad2394e666956f5dfd7a57c143d4d2061d
guxueX64 <wzy88335256@126.com>
2024-10-01 20:24:40 +0800

    Update table.js

--- db/table.js
+++ db/table.js
@@ -1,14 +1,14 @@
 //Define an object named fundraiser to represent relevant information about fundraising activities
 const fundraiser = {
     name: 'fundraiser', 
-    fields: ['fundraiser_id', 'organizer', 'caption','target_funding',
+    fieds: ['fundraiser_id', 'organizer', 'caption','target_funding',
     'current_funding','city','active','category_id','fundraiser_image'], 
 };
 
 //Define an object named category to represent relevant information about the category
 const category = {
     name: 'category', 
-    fields: ['category_id', 'name'], 
+    fieds: ['category_id', 'name'], 
 };
 
 //Export funder and category objects
